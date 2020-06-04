5263a7be77ef8b265ae9b14a7a3e7f84632e88b7
Harshil Bavaria <harshilbb@gmail.com>
2020-06-04 12:14:25 -0500

    minor formatting changes

--- Project.js
+++ Project.js
@@ -3,58 +3,41 @@ function main(){
     const regedit = require('regedit')
     const fs = require('fs')
     const path = require('path')
-    //const csvTojson = require('csvtojson')
     const csvjson = require('csvJson')
 
     const regPath = 'HKLM\\SOFTWARE\\WOW6432Node\\Emerson\\CIF'
     // Listing all the objects from that registry
     regedit.list(regPath, function(err,result){
-    //Reading justh the value(path) of the INstallation of DeltaV
+    //Reading just the value(path) of the INstallation of DeltaV
     defaultValue = result[regPath].values['DATA_DIRECTORY'].value
     processFolder(defaultValue)
-    //console.log(finalResult)
-    //sendData(finalResult)
     })
 
 async function processFolder(defaultValue){
-    //let results = []
     // Merging the path and some of the subfolder which were not in the path and are common on all devices
     let folder = defaultValue + '\\Performance'
     if (!fs.existsSync(folder)){
         console.log("no dir ",folder);
         return;
     }
-    //await readDirectory(folder)
-    //sendData(finalResult)
     finalResults = await readDirectory(folder)
-    //console.log(finalResults) 
     sendData(finalResults)
 }
 
 async function readDirectory(folder){
     let results = []
-    //let i = 0
     // Reading all the files in the directory and finding '.csv' extension and then read it and convert it to json string  
     let files=fs.readdirSync(folder)
     for(let i=0;i<files.length;i++){
         let fileName=path.join(folder,files[i])
         let stat = fs.lstatSync(fileName)
         if (stat.isDirectory()){
-            //combine two arrays (result and one declared in  this function)
             results = results.concat((await readDirectory(fileName))) //recurse
-            //await readDirectory(fileName)
         }
         else if (fileName.indexOf('.csv')>=0) {
-            //i = i + 1
-        
-            // Calling the funtion to read that file.
              results.push(await readPerformancefile(fileName))
-             //await readPerformancefile(fileName)
         }
-        //i = i+1
     }
-    //console.log(results)
-    //sendData(results)
     return results
 }
 
@@ -64,15 +47,13 @@ async function readPerformancefile(fileName){
           if (err) {
             reject(err);
           }
-          var options = {
-            delimiter : ',', // optional
-            quote     : '"' // optional
+          var formatting = {
+            delimiter : ',',
+            quote     : '"'
           }
-          let jsonObj = csvjson.toObject(data, options);
+          let jsonObj = csvjson.toObject(data, formatting);
           resolve(jsonObj);
-          //console.log(jsonObj)
           return jsonObj
-          //console.log(jsonObj)
     });
     })
 }
@@ -86,10 +67,7 @@ async function sendData(finalResults){
     }, function (error, response, body){
         console.log(`statusCode: ${response.statusCode}`)
      });
-     //let parseData = Json.parse(finalResults)
-     //console.log(parseData)
-     console.log(finalResults)
-    //console.log(jsonObj[0][' Run Group'])
+     console.log(finalResults[0][0].Session)
 }
 }
 main();
