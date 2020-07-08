1ffd4d3172affecaafb36adfcc9efd736c1a6512
Harshil Bavaria <harshilbb@gmail.com>
2020-07-08 08:29:47 -0500

    Just changed the link in post request

--- Project.js
+++ Project.js
@@ -63,7 +63,7 @@ function main(){
     convertTonumbers(finalResults)
     convertTodate(finalResults)  
     request({
-        url: "http://localhost:4000/StorePerformanceData/todos",
+        url: "http://10.223.31.195:4000/StorePerformanceResults/todos",
         method: "POST",
         json: true, 
         body: finalResults
